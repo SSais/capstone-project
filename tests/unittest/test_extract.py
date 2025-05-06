18d8d10a572ae499de47adcbd7faf6eb291f275e
Sayed <sayed.sais@gmail.com>
2025-05-06 17:11:23 +0100

    remove set-up tests, add requirements.txt, add extract code and tests

--- tests/unittest/test_extract.py
+++ tests/unittest/test_extract.py
@@ -1,11 +1,29 @@
-from etl.extract.extract import pytest_confirm
+import pytest
+from unittest.mock import patch
+
+from etl.extract.extract import get_request_coingecko_api
+
+mock_api_key = 123
 
 
-def test_pytest():
+# Test get request to coingecko api
+def test_get_request_coingecko_api():
     # Arrange
-    input = 1
-    expected_output = 2
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = '{api-data}'
+        # Action
+        response = get_request_coingecko_api(mock_api_key)
+        # Assert
+        assert response == '{api-data}'
+
+
+# Test get request exception
+def test_get_request_exception():
+    # Arrange
+    expected_error = 'An unexpected error occurred'
     # Action
-    actual_output = pytest_confirm(input)
+    with pytest.raises(Exception) as msg:
+        get_request_coingecko_api(mock_api_key)
     # Assert
-    assert actual_output == expected_output
+    assert expected_error in str(msg.value)
