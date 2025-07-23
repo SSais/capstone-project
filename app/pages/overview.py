9077d326ded8271c330ff1ce7f0288a6f621800b
Sayed <sayed.sais@gmail.com>
2025-07-23 12:50:26 +0000

    adjust heatmap

--- app/pages/overview.py
+++ app/pages/overview.py
@@ -1,33 +1,39 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import numpy as np
 
 # Get data from session state
 complete_ticker_df = st.session_state['daily_data']
 company_df = st.session_state['company_data']
 
-# list tickers
+# List tickers
+TICKERS = [
+    'ARGX',  # Argenx
+    'GMAB',  # Genmab
+    'AAPL',  # Apple
+    'IBM',   # IBM
+    ]
 
-# for each ticker filter for symbol and then select the last returns p (which will be columns 8)
+# For each ticker filter for symbol and then select the last returns p
+for ticker in TICKERS:
+    # Filter for the current ticker
+    ticker_data = complete_ticker_df[complete_ticker_df['symbol'] == ticker]
 
-# add those last numbers to the 
-
-data_argenx_today = data_argenx.iloc[-1, 8]
-data_gmab_today = data_gmab.iloc[-1, 8]
-
-data_overview.loc[data_overview['Symbol'] == 'ARGX', 'returns_p'] = data_argenx_today
-data_overview.loc[data_overview['Symbol'] == 'GMAB', 'returns_p'] = data_gmab_today
+    # Get the latest returns_p
+    last_return_p = ticker_data['daily_return_p'].iloc[-1]
+        
+    # Update the 'returns_p' in data_overview for the corresponding symbol
+    company_df.loc[company_df['Symbol'] == ticker, 'daily_return_p'] = last_return_p
 
 
 fig = px.treemap(
-    data_overview,
+    company_df,
     path=['Sector', 'Symbol'],
     values='MarketCapitalization',
-    color='returns_p',
+    color='daily_return_p',
     color_continuous_midpoint=0,
     color_continuous_scale='RdYlGn',
-    custom_data=['returns_p']
+    custom_data=['daily_return_p']
 )
 
 fig.update_layout(coloraxis_showscale=False)
